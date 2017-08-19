4aaf8c32f2afe45adde4e0919981512445b1cf5e
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-08-19 18:49:51 -0300

    Destroys enemy creature

--- Assets/Scripts/Player/CreatureController.cs
+++ Assets/Scripts/Player/CreatureController.cs
@@ -8,15 +8,17 @@ public class CreatureController : MonoBehaviour {
 	public Slider healthSlider;
 	public Image fillSliderImage;
 	public int belongsToPlayer;
-	public float speed = 1.0f;
+	public float speed = 0.1f;
 
 	[HideInInspector] public bool moved;
 	[HideInInspector] public bool isTired;
 
 	private int health = 10;
+	private bool finishedInteractionAnimation = false;
 	private GameObject creatureModel;
 	private ParticleSystem explosionParticles;
 	private ParticleSystem rocksParticles;
+	private CreatureController enemy;
 
 	void Awake()
 	{
@@ -26,8 +28,13 @@ public class CreatureController : MonoBehaviour {
 		creatureModel = gameObject;
 		animatorController.SetTrigger ("IsIdle");
 
-		explosionParticles = Instantiate (GameManager.instance.boardScript.explosionParticles, transform.position, Quaternion.identity);
-		rocksParticles = Instantiate (GameManager.instance.boardScript.rockExplorationParticles, transform.position, Quaternion.identity);
+		explosionParticles = Instantiate (GameManager.instance.boardScript.explosionParticles, transform.position, Quaternion.identity, transform);
+		rocksParticles = Instantiate (GameManager.instance.boardScript.rockExplorationParticles, transform.position, Quaternion.identity, transform);
+	}
+
+	public void FinishedAnimation()
+	{
+		finishedInteractionAnimation = true;
 	}
 
 	public void TakeDamage(int damage)
@@ -36,7 +43,7 @@ public class CreatureController : MonoBehaviour {
 		if(health<=0)
 		{
 			health = 0;
-			Die ();
+			//Die ();
 		}
 		healthSlider.value = health;
 	}
@@ -65,6 +72,18 @@ public class CreatureController : MonoBehaviour {
 		newCreature.MoveToTarget (target);
 	}
 
+	public void Attack(Transform origin, Transform target, ref CreatureController enemy)
+	{
+		this.enemy = enemy;
+
+		enemy.transform.rotation = Quaternion.LookRotation (origin.position - target.position);
+
+		Vector3 walkingPosition = target.position - ((target.position - origin.position) / GameManager.instance.boardScript.tiles.tileSideSize);
+
+		StartCoroutine (Attacking (origin, target, walkingPosition));
+
+	}
+
 	public void Explore()
 	{
 		explosionParticles.Play ();
@@ -76,9 +95,12 @@ public class CreatureController : MonoBehaviour {
 		explosionParticles.Play ();
 	}
 
-	private void Die ()
+	public void Die (CreatureController killer)
 	{
-		
+		// TODO: Play animation
+
+		killer.FinishedAnimation ();
+		Destroy (gameObject);
 	}
 
 	private IEnumerator Moving(Transform target)
@@ -90,10 +112,41 @@ public class CreatureController : MonoBehaviour {
 			yield return null;
 		}
 		animatorController.SetTrigger ("IsIdle");
+
+
 		transform.rotation = Quaternion.identity;
 		ActionsManager.instance.FinishAction ();
 
-		explosionParticles.transform.position = transform.position;
-		rocksParticles.transform.position = transform.position;
+		/*explosionParticles.transform.position = transform.position;
+		rocksParticles.transform.position = transform.position;*/
+	}
+
+	private IEnumerator Attacking(Transform origin, Transform target, Vector3 midTarget)
+	{
+		animatorController.SetTrigger ("Moves");
+
+		transform.rotation = Quaternion.LookRotation (midTarget - transform.position);
+		while((transform.position - midTarget).sqrMagnitude > 0.15)
+		{
+			transform.position = Vector3.Lerp (transform.position, midTarget, Time.deltaTime * speed);
+			yield return null;
+		}
+			
+		animatorController.SetTrigger ("Attacks");
+		finishedInteractionAnimation = false;
+		while(!finishedInteractionAnimation)
+		{
+			yield return null;
+		}
+
+		animatorController.SetTrigger ("IsIdle");
+		finishedInteractionAnimation = false;
+		enemy.Die (this);
+		while(!finishedInteractionAnimation)
+		{
+			yield return null;
+		}
+
+		MoveToTarget (target);
 	}
 }
