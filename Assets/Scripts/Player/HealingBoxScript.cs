aa8e7d4ea00adf800fdbc2d7bf32b2c429072ab7
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-08-25 20:05:58 -0300

    Adds prefab for healing box

---
+++ Assets/Scripts/Player/HealingBoxScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingBoxScript : MonoBehaviour {
+
+	CreatureController parent;
+
+	void Start()
+	{
+		parent = GetComponentInParent <CreatureController> ();
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log ("Clicked Healing Box!");
+		parent.animatorController.SetTrigger ("Moves");
+	}
+
+	void OnMouseOver()
+	{
+		// TODO: Make creature plead for healing.
+	}
+}
