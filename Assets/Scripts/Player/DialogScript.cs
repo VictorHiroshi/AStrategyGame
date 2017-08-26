d9397dde20edd85409d4448016f2350330ba2f3b
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-08-25 22:27:29 -0300

    Fading balloon on mouse over

---
+++ Assets/Scripts/Player/DialogScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class DialogScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	private Image balloon;
+	private Text dialogMessage;
+
+	void Awake()
+	{
+		balloon = GetComponent <Image> ();
+		dialogMessage = GetComponentInChildren <Text> ();
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		Color temp = balloon.color;
+		temp.a = temp.a/4;
+		balloon.color = temp;
+
+		temp = dialogMessage.color;
+		temp.a = temp.a/4;
+		dialogMessage.color = temp;
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Color temp = balloon.color;
+		temp.a = temp.a*4;
+		balloon.color = temp;
+
+		temp = dialogMessage.color;
+		temp.a = temp.a*4;
+		dialogMessage.color = temp;
+	}
+}
